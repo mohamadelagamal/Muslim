5e97ef86cedb2e0cbd1b29821808e84f1d5f34ca
omar elsadany <omarelsadany33@gmail.com>
2023-08-11 18:42:27 +0300

    this contains Quran screen

---
+++ app/src/main/java/com/example/muslim/model/SurahInfoItem.kt
@@ -0,0 +1,12 @@
+package com.example.muslim.model
+
+data class SurahInfoItem(
+    val count: Int,
+    val index: String,
+    val juz: List<Juz>,
+    val pages: String,
+    val place: String,
+    val title: String,
+    val titleAr: String,
+    val type: String
+)
